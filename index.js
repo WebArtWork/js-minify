372e1a516fa0c29c5f7da09c30088e291b7bafd7
Honchar Denys <crackeraki@gmail.com>
2017-08-31 19:04:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ var ngAnnotate = require('gulp-ng-annotate');
 var fs = require('fs');
 
 module.exports = function gulpJsMinify(config) {
+	var name = 'lab';
+	if(config.name) name = config.name;
 	if(config.production) var files = config.productionFiles;
 	else var files = config.files;
-	if (fs.existsSync(config.way + 'lab-min.js')) {
-		var data = fs.readFileSync(config.way + 'lab-min.js', 'utf8');
+	if (fs.existsSync(config.way + name + '-min.js')) {
+		var data = fs.readFileSync(config.way + name + '-min.js', 'utf8');
 		var lines = data.split("\n");
 		if (lines[lines.length - 1].indexOf(files.toString() + config.way + config.prefix) > -1) return;
 	}
 	gulp.src(files)
 	.pipe(sourcemaps.init())
-	.pipe(concat('lab.js'))
+	.pipe(concat(name + '.js'))
 	.pipe(ngAnnotate())
 	.pipe(sourcemaps.write())
 	.pipe(minify({
@@ -24,6 +26,6 @@ module.exports = function gulpJsMinify(config) {
 	}))
 	.pipe(gulp.dest(config.way))
 	.pipe(gulpFn(function() {
-		fs.appendFile(config.way + 'lab-min.js', '\r\n/*' + files.toString() + config.way + config.prefix + '*/', function(err) {});
+		fs.appendFile(config.way + name + '-min.js', '\r\n/*' + files.toString() + config.way + config.prefix + '*/', function(err) {});
 	}));
 };
